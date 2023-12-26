9384d189322b4edff78c496884fa22b66eb33ca8
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2023-12-26 12:14:19 +0100

    feat: set sls ft to yaml

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,3 +4,5 @@ local highlight_group = vim.api.nvim_create_augroup('YankHighlight', {clear = tr
 vim.api.nvim_create_autocmd('TextYankPost',
                             {callback = function() vim.highlight.on_yank() end, group = highlight_group, pattern = '*'})
 
+-- set sls files filetype to yaml
+vim.api.nvim_create_autocmd({'BufNewFile','BufRead'}, { pattern = '*.sls', callback = function() vim.bo.filetype = 'yaml' end})
