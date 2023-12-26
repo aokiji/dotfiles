52e8ad48b841930d67ad55aa13bddb3c005dfd70
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2023-12-26 12:24:15 +0100

    feat: set sw,et,ts for sls

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,4 +5,13 @@ vim.api.nvim_create_autocmd('TextYankPost',
                             {callback = function() vim.highlight.on_yank() end, group = highlight_group, pattern = '*'})
 
 -- set sls files filetype to yaml
-vim.api.nvim_create_autocmd({'BufNewFile','BufRead'}, { pattern = '*.sls', callback = function() vim.bo.filetype = 'yaml' end})
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+  pattern = '*.sls',
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_option(buf, "filetype", "yaml")
+    vim.api.nvim_buf_set_option(buf, "expandtab", true)
+    vim.api.nvim_buf_set_option(buf, "shiftwidth", 2)
+    vim.api.nvim_buf_set_option(buf, "tabstop", 2)
+  end
+})
