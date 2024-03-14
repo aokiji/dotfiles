d048b78570dd151462cdc64b1e83fd59e26997c1
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2024-03-14 19:39:31 +0100

    feat: setup trouble

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -7,10 +7,18 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = function()
       local actions = require('telescope.actions')
+      local trouble = require("trouble.providers.telescope")
 
       return {
         defaults = {
-          mappings = { i = { ['<C-j>'] = actions.move_selection_next, ['<C-k>'] = actions.move_selection_previous } }
+          mappings = {
+            i = {
+              ['<C-j>'] = actions.move_selection_next,
+              ['<C-k>'] = actions.move_selection_previous,
+              ["<c-t>"] = trouble.smart_open_with_trouble
+            },
+            n = { ["<c-t>"] = trouble.smart_open_with_trouble },
+          }
         },
         pickers = {
           buffers = { ignore_current_buffer = true, sort_lastused = true },
