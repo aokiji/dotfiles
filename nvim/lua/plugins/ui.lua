22607425075d5c269305a02ac06b1de3d4f33a2a
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2024-10-10 17:30:34 +0200

    fix: Trouble mappings

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -70,12 +70,37 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
     keys = {
-      { "<leader>xx", function() require("trouble").toggle() end,                        desc = 'Toggle Trouble' },
-      { "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, desc = 'Trouble Workspace Diagnostics' },
-      { "<leader>xd", function() require("trouble").toggle("document_diagnostics") end,  desc = 'Trouble Document Diagnostics' },
-      { "<leader>xq", function() require("trouble").toggle("quickfix") end,              desc = 'Trouble Quickfix' },
-      { "<leader>xl", function() require("trouble").toggle("loclist") end,               desc = 'Trouble Loclist' },
-      { "gR",         function() require("trouble").toggle("lsp_references") end,        desc = 'Trouble Lsp References' }
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+      { "gR", function() require("trouble").toggle("lsp_references") end, desc = 'Trouble Lsp References' }
     }
   },
   -- better vim.ui.select and vim.ui.input
