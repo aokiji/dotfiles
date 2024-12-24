2761c912e165c40b18081f385c61eaa7a7cb8e2f
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2024-12-24 16:57:37 +0100

    fix: missing file

---
+++ nvim/lua/config/filetypes.lua
@@ -0,0 +1,14 @@
+vim.filetype.add({
+  filename = {
+    ["docker-compose.yml"] = "yaml.docker-compose",
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["compose.yml"] = "yaml.docker-compose",
+    ["compose.yaml"] = "yaml.docker-compose",
+  },
+})
+
+vim.filetype.add({
+  pattern = {
+    [".*%.pg"] = "sql"
+  }
+})
