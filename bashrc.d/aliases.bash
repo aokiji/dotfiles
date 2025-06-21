3f3775834fa58baea1ef904afaa6aa82d96d5d6c
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2025-06-21 17:37:58 +0200

    feat: kubectl aliases by context

--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -5,3 +5,5 @@ alias ll="ls -al"
 # Pretty print the path
 alias path='echo $PATH | tr -s ":" "\n"'
 alias rg='rg --type-add "batch:*.cmd"'
+alias kp='kubectl --context=user-dyo-context'
+alias ks='kubectl --context=user-dyo-staging-context'
