9913a8fee84c0b8b88bdef42504116d5c4fa909c
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2025-06-25 15:57:56 +0200

    fix: clangd configuration

--- nvim/lua/plugins/coding/lsp.lua
+++ nvim/lua/plugins/coding/lsp.lua
@@ -54,7 +54,6 @@ return {
           cmd = { '/opt/uv/cpython-3.13.3-linux-x86_64-gnu/bin/ruff', 'server' }
         },
         clangd = {
-          root_dir = function(_) end
         },
         cmake = {
 
