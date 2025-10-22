f491c21343e4868c9a3517637f8840d1697364fc
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2025-10-22 08:31:52 +0200

    enh(leap): jump up and down with s

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -235,19 +235,13 @@ return {
   {
     "ggandor/leap.nvim",
     opts = { safe_labels = {} },
-    keys = {
-      { "s",  mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-      { "S",  mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-      { "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
-    },
     config = function(_, opts)
       local leap = require("leap")
       for k, v in pairs(opts) do
         leap.opts[k] = v
       end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ "x", "o" }, "x")
-      vim.keymap.del({ "x", "o" }, "X")
+      vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+      vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
     end,
   }
 }
