3bcb6ee01b5f23438e11337fc15da97b3669d587
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2026-03-21 21:50:40 +0100

    feat: redmine queries

--- nvim/lua/plugins/coding/init.lua
+++ nvim/lua/plugins/coding/init.lua
@@ -153,6 +153,7 @@ return {
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
