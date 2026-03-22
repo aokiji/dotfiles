2a86608e5d68271a0cbe9b3a46a9de57f4b0e211
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2026-03-22 21:52:42 +0100

    fix: tv init appended to bashrc every time

--- bashrc.d/television.bash
+++ bashrc.d/television.bash
@@ -1,5 +1,6 @@
-echo 'eval "$(tv init bash)"' >>~/.bashrc
+eval "$(tv init bash)"
 export TELEVISION_CONFIG=$DOTFILES_ROOT_DIR/television
+
 if [ -n "$TMUX" ]; then
 	tmux set-environment -g TELEVISION_CONFIG "$TELEVISION_CONFIG"
 fi
