eb4d352297f4d90f421e96117bc44052d7cce9d8
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2026-03-22 21:40:44 +0100

    feat: tmux sessions through tv

--- bashrc.d/television.bash
+++ bashrc.d/television.bash
@@ -1,2 +1,5 @@
-echo 'eval "$(tv init bash)"' >> ~/.bashrc
+echo 'eval "$(tv init bash)"' >>~/.bashrc
 export TELEVISION_CONFIG=$DOTFILES_ROOT_DIR/television
+if [ -n "$TMUX" ]; then
+	tmux set-environment -g TELEVISION_CONFIG "$TELEVISION_CONFIG"
+fi
