1ea3e7bc7161ccc6d6611dc226e351eefb7cf438
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2026-03-25 21:43:39 +0100

    fix: postgres_lsp not starting

--- nvim/lsp/postgres_lsp.lua
+++ nvim/lsp/postgres_lsp.lua
@@ -1,5 +1,6 @@
 return {
   -- cmd = { "cargo", "run", "--bin", "postgrestools", "lsp-proxy", "--log-path", "/tmp/postgres_lsp.log" },
-  cmd = { "postgrestools", "lsp-proxy", "--log-path", "/tmp/postgres_lsp.log"}
+  cmd = { "postgrestools", "lsp-proxy", "--log-path", "/tmp/postgres_lsp.log"},
+  workspace_required = false
   -- cmd_cwd = "/home/nicolas.delossantos/workspace/postgres-language-server",
 }
