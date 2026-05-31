0b042592d942e94de1abf87cd40c6848ba3879ca
Nicolás De los Santos <nicolas.delossantos@meteologica.com>
2026-05-31 16:33:10 +0200

    feat(nvim/tv): navigate tree sitter tags

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -121,14 +121,14 @@ return {
         channels = {
           -- `files`: fuzzy find files in your project
           files = {
-            layout = "portrait", --- override global setting for this channel
+            layout = "portrait",       --- override global setting for this channel
             keybinding = "<leader>tf", -- Launch the files channel
             -- what happens when you press a key
             handlers = {
-              ["<CR>"] = h.open_as_files, -- default: open selected files
-              ["<C-q>"] = h.send_to_quickfix, -- send to quickfix list
-              ["<C-s>"] = h.open_in_split, -- open in horizontal split
-              ["<C-v>"] = h.open_in_vsplit, -- open in vertical split
+              ["<CR>"] = h.open_as_files,      -- default: open selected files
+              ["<C-q>"] = h.send_to_quickfix,  -- send to quickfix list
+              ["<C-s>"] = h.open_in_split,     -- open in horizontal split
+              ["<C-v>"] = h.open_in_vsplit,    -- open in vertical split
               ["<C-y>"] = h.copy_to_clipboard, -- copy paths to clipboard
             },
           },
@@ -137,13 +137,20 @@ return {
           text = {
             keybinding = "<leader>tt",
             handlers = {
-              ["<CR>"] = h.open_at_line, -- Jump to line:col in file
-              ["<C-q>"] = h.send_to_quickfix, -- Send matches to quickfix
-              ["<C-s>"] = h.open_in_split, -- Open in horizontal split
-              ["<C-v>"] = h.open_in_vsplit, -- Open in vertical split
+              ["<CR>"] = h.open_at_line,       -- Jump to line:col in file
+              ["<C-q>"] = h.send_to_quickfix,  -- Send matches to quickfix
+              ["<C-s>"] = h.open_in_split,     -- Open in horizontal split
+              ["<C-v>"] = h.open_in_vsplit,    -- Open in vertical split
               ["<C-y>"] = h.copy_to_clipboard, -- Copy matches to clipboard
             },
-          },          -- `git-branch`: browse git branches
+          },
+          ["tree_sitter_tags"] = {
+            keybinding = "<leader>tT",
+            handlers = {
+              ["<CR>"] = h.open_at_line,
+            }
+          },
+          -- `git-branch`: browse git branches
           ["git-branch"] = {
             keybinding = "<leader>tb",
             handlers = {
